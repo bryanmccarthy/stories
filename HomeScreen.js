cd858e2815d8a90ea57d927bde548174ab9c9ef0
Bryan McCarthy <bryanmccarthy2@gmail.com>
2023-05-17 11:08:32 +0100

    bg image and filter wip

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,8 +1,8 @@
-import { useNavigation } from '@react-navigation/native';
 import { useLayoutEffect } from 'react';
-import { ScrollView, View, Text, SafeAreaView, Button, TextInput } from 'react-native';
+import { ScrollView, View, Text, SafeAreaView, TextInput, ImageBackground } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
-
+// {/* Button could be Lock Icon if unavailable to user */}
+            // {/* <Ionicons name="lock-closed-outline" size={32} color="black" /> */}
 const stories = [
   {
     id: 1,
@@ -51,33 +51,35 @@ function HomeScreen({ navigation }) {
   }, []);
 
   return (
-    <SafeAreaView className="bg-indigo-50">
+    <SafeAreaView className="bg-indigo-100">
       {/* Header */}
-      <View className="flex flex-row justify-between items-center px-2 py-1 bg-indigo-300">
+      <View className="flex flex-row justify-between items-center px-2 py-1 bg-indigo-100">
         <Text className="text-xl">Stories</Text>
         <Ionicons name="person-circle-outline" size={32} color="black" />
       </View>
-      {/* Search */}
-      <View className="relative flex flex-row items-center px-2 py-2">
-        <Ionicons name="search-outline" size={18} color="black" />
-        <TextInput className="absolute w-32 px-7" placeholder="Search" />
+      {/* Search & filters */}
+      <View className="relative flex flex-row border-solid border rounded-2xl h-8 px-2 items-center border-black mt-1 mx-1 bg-indigo-100">
+          <Ionicons name="search-outline" size={18} color="black" />
+          <TextInput className="absolute w-40 px-7 py-2" placeholder="Search" />
+          <View className="absolute right-1 flex flex-row">
+            <Ionicons name="filter-outline" size={18} color="black" />
+            <Text className="text-sm px-1">Filters</Text>
+          </View>
       </View>
       {/* Stories */}
       <ScrollView contentContainerStyle={styles.wrapper}>
         {stories.map(story => (
-          <View key={story.id} className="relative border-solid border-2 rounded-lg w-44 h-44 m-2 bg-indigo-300">
-            {/* IMAGE */}
-            <Text className="text-black text-sm">{story.name}</Text>
-            {/* Button could be Lock Icon if unavailable to user */}
-            {/* <Ionicons name="lock-closed-outline" size={32} color="black" /> */}
-            <View className="absolute right-2 -bottom-3 border-solid border-2 rounded-full px-2 pt-2 pb-1 bg-slate-200">
-            <Ionicons
-              name="book"
-              size={28} 
-              color="rgb(49 46 129)"
-              onPress={() => navigation.navigate('Story', { id: story.id, name: story.name  })}
-            />
-            </View>
+          <View key={story.id} className="relative shadow-md shadow-slate-400 w-40 h-40 m-3">
+            <ImageBackground source={require('./assets/elephant.jpeg')} style={{ width: '100%', height: '100%' }} imageStyle={{ borderRadius: 8 }}>
+              <View className="absolute right-1.5 -bottom-4 border-solid border rounded-full px-2 pt-2 pb-1 bg-indigo-100">
+              <Ionicons
+                name="book"
+                size={28} 
+                color="black"
+                onPress={() => navigation.navigate('Story', { id: story.id, name: story.name  })}
+              />
+              </View>
+            </ImageBackground>
           </View>
         ))}
       </ScrollView>
