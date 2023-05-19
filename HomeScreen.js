e42251567dce7400e54668060cdc9b875000820c
Bryan McCarthy <bryanmccarthy2@gmail.com>
2023-05-19 20:45:35 +0100

    resize covers

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,48 +1,57 @@
 import { useLayoutEffect } from 'react';
 import { ScrollView, View, Text, SafeAreaView, TextInput, ImageBackground } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
-// {/* Button could be Lock Icon if unavailable to user */}
-            // {/* <Ionicons name="lock-closed-outline" size={32} color="black" /> */}
+
 const stories = [
   {
     id: 1,
     name: 'The little ant',
+    cover: require('./assets/1.jpeg')
   },
   {
     id: 2,
     name: 'Henry the mouse',
+    cover: require('./assets/2.jpeg')
   },
   {
     id: 3,
     name: 'The little ant',
+    cover: require('./assets/3.png')
   },
   {
     id: 4,
     name: 'Henry the mouse',
+    cover: require('./assets/4.jpeg')
   },
   {
     id: 5,
     name: 'The little ant',
+    cover: require('./assets/5.jpeg')
   },
   {
     id: 6,
     name: 'Henry the mouse',
+    cover: require('./assets/6.jpeg')
   },
   {
     id: 7,
     name: 'The little ant',
+    cover: require('./assets/7.jpeg')
   },
   {
     id: 8,
     name: 'Henry the mouse',
+    cover: require('./assets/8.png')
   },
   {
     id: 9,
     name: 'The little ant',
+    cover: require('./assets/9.png')
   },
   {
     id: 10,
     name: 'Henry the mouse',
+    cover: require('./assets/10.jpeg')
   },
 ];
 
@@ -62,9 +71,9 @@ function HomeScreen({ navigation }) {
         <Ionicons name="person-circle-outline" size={44} color="black" />
       </View>
       {/* Search & filters */}
-      <View className="relative flex flex-row border-solid border rounded-2xl h-8 px-2 items-center border-black mt-1 mx-1 bg-indigo-100">
+      <View className="relative flex flex-row border-solid border rounded-2xl h-8 my-2 px-2 items-center border-black mt-1 mx-1 bg-indigo-100">
           <Ionicons name="search-outline" size={18} color="black" />
-          <TextInput className="absolute w-40 px-7 py-2" placeholder="Search" />
+          <TextInput className="absolute w-64 px-7 py-2" placeholder="Search" />
           <View className="absolute right-1 flex flex-row">
             <Ionicons name="filter-outline" size={18} color="black" />
             <Text className="text-sm px-1">Filters</Text>
@@ -73,9 +82,9 @@ function HomeScreen({ navigation }) {
       {/* Stories */}
       <ScrollView contentContainerStyle={styles.wrapper}>
         {stories.map(story => (
-          <View key={story.id} className="relative shadow-md shadow-slate-400 w-40 h-40 m-3">
-            <ImageBackground source={require('./assets/elephant.jpeg')} style={{ width: '100%', height: '100%' }} imageStyle={{ borderRadius: 8 }}>
-              <View className="absolute right-1.5 -bottom-4 rounded-full px-2 pt-2 pb-1 bg-indigo-100">
+          <View key={story.id} className="relative shadow-md shadow-slate-400 w-40 h-52 m-3">
+            <ImageBackground source={story.cover} style={{ width: '100%', height: '100%' }} imageStyle={{ borderRadius: 8 }}>
+              <View className="absolute right-2 -bottom-4 rounded-full px-2 pt-2 pb-1 bg-indigo-100">
               <Ionicons
                 name="book"
                 size={28} 
@@ -86,8 +95,6 @@ function HomeScreen({ navigation }) {
             </ImageBackground>
           </View>
         ))}
-        {/* Scroll room */}
-        <View className="h-72"></View>
       </ScrollView>
     </SafeAreaView>
   );
@@ -100,6 +107,7 @@ const styles = {
     flexDirection: 'row',
     flexWrap: 'wrap',
     justifyContent: 'center',
+    paddingBottom: 120,
   },
 };
 
