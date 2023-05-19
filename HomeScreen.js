3a8fdd0bf802e4e0ed7680a963202b9fcdea66d4
Bryan McCarthy <bryanmccarthy2@gmail.com>
2023-05-19 23:01:55 +0100

    info modal

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,6 +1,7 @@
 import { useLayoutEffect } from 'react';
-import { ScrollView, View, Text, SafeAreaView, TextInput, ImageBackground } from 'react-native';
+import { ScrollView, View, Text, SafeAreaView, TextInput, ImageBackground, Modal, TouchableWithoutFeedback } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import { useState } from 'react';
 
 const stories = [
   {
@@ -56,6 +57,7 @@ const stories = [
 ];
 
 function HomeScreen({ navigation }) {
+  const [modalVisible, setModalVisible] = useState(false);
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -65,6 +67,26 @@ function HomeScreen({ navigation }) {
 
   return (
     <SafeAreaView className="bg-indigo-100 w-full h-full">
+      {/* Modal */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(false);
+        }
+      }>
+        <TouchableWithoutFeedback onPress={() => setModalVisible(false)}>
+        <View className="flex flex-col justify-center items-center h-full">
+          <View className="flex flex-col justify-center items-center bg-white w-11/12 h-1/2 rounded-2xl">
+            <Text className="text-2xl font-bold mt-4">Story Info</Text>
+            <Text className="text-lg font-bold mt-4">Title</Text>
+            <Text className="text-lg font-bold mt-4">Genre</Text>
+            <Text className="text-lg font-bold mt-4">Description</Text>
+          </View>
+        </View>
+        </TouchableWithoutFeedback>
+      </Modal>
       {/* Header */}
       <View className="flex flex-row justify-between items-center px-2 py-1 bg-indigo-100">
         <Text className="text-2xl">Stories</Text>
@@ -89,13 +111,23 @@ function HomeScreen({ navigation }) {
         {stories.map(story => (
           <View key={story.id} className="relative shadow-md shadow-slate-400 w-40 h-52 m-4 mb-6">
             <ImageBackground source={story.cover} style={{ width: '100%', height: '100%' }} imageStyle={{ borderRadius: 8 }}>
-              <View className="absolute right-2 -bottom-4 rounded-full px-2 pt-2 pb-1 bg-indigo-100">
-              <Ionicons
-                name="book"
-                size={28} 
-                color="black"
-                onPress={() => navigation.navigate('Story', { id: story.id, title: story.title  })}
-              />
+              {/* Info Icon */}
+              <View className="absolute -bottom-4 right-14 rounded-full px-2 pt-2 pb-1 bg-white">
+                <Ionicons
+                  name="help-outline"
+                  size={28}
+                  style={{ color: 'black' }}
+                  onPress={() => setModalVisible(true)}
+                />
+              </View>
+              {/* Book Icon */}
+              <View className="absolute right-2 -bottom-4 rounded-full px-2 pt-2 pb-1 bg-white">
+                <Ionicons
+                  name="book"
+                  size={28} 
+                  style={{ color: 'black' }}
+                  onPress={() => navigation.navigate('Story', { id: story.id, title: story.title  })}
+                />
               </View>
             </ImageBackground>
           </View>
